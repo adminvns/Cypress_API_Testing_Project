ff3be65582e1c5d4ae7dcd10c3e50c3ac9a44284
Shubham_admin <shubham.editor@gmail.com>
2024-09-16 21:52:32 +0530

    test

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       console.log(config.env)
-      const envName = config.env.name;
+      const envName = config.env.name || 'dev';
       const text = readFileSync(`environment_configs/${envName}.json`);
       const values = JSON.parse(text);
       config.env = {
